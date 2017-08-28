c3d37e151dd9ecd182626950d3ebd4054886834c
Gabriel Rodrigues <gabriel.rodrigues@apesoftware.com.br>
2017-08-27 21:13:49 -0300

    configuracoes do automapper ao projeto foram adicionadas.

---
+++ WebApplication/Infraestrutura/Config/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using WebApplication.Infraestrutura.Profiles;
+
+namespace WebApplication.Infraestrutura.Config
+{
+    public class AutoMapperConfig
+    {
+        public static IMapper CreateMapper()
+        {
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<EmpregadoProfile>();
+            });
+
+
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ WebApplication/Infraestrutura/Profiles/EmpregadoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Domain;
+using WebApplication.Models;
+
+namespace WebApplication.Infraestrutura.Profiles
+{
+    public class EmpregadoProfile : Profile
+    {
+        public EmpregadoProfile()
+        {
+            CreateMap<Empregado, EmpregadoViewModel>()
+                .ReverseMap();
+        }
+    }
+}
